9890d655aafcb68611180969cd4c1328d5ff1f89
Lauren Leslie Fereira <laurenlesliesilva@gmail.com>
2023-09-03 01:00:14 -0300

    fix: update coffee route

--- server/src/routes/coffeesRoutes.ts
+++ server/src/routes/coffeesRoutes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
-import { CoffeeSchema } from "../schemas/coffee";
+import { CreateCoffeeSchema, UpdateCoffeeSchema } from "../schemas/coffee";
 
 interface UpdateCoffeeRequest {
   id: string;
@@ -27,7 +27,7 @@ export async function CoffeesRoutes(app: FastifyInstance) {
   });
 
   app.post("/coffees/new", async (req, res) => {
-    const { title, description, price, typeName } = CoffeeSchema.parse(
+    const { title, description, price, typeName } = CreateCoffeeSchema.parse(
       req.body
     );
 
@@ -61,37 +61,58 @@ export async function CoffeesRoutes(app: FastifyInstance) {
 
   app.put("/coffees/:id", async (req, res) => {
     const { id } = req.params as UpdateCoffeeRequest;
-    const { title, description, price, typeName } = CoffeeSchema.parse(
-      req.body
-    );
 
     try {
-      const existingType = await prisma.type.findUnique({
+      // Obter o café existente no banco de dados
+      const existingCoffee = await prisma.coffee.findUnique({
         where: {
-          title: typeName,
+          id,
         },
       });
 
-      if (!existingType) {
-        return res
-          .status(400)
-          .send({ error: "The coffee type informed does not exist." });
+      if (!existingCoffee) {
+        return res.status(404).send({ error: "Coffee not found." });
       }
 
+      // Extrair as propriedades que podem ser atualizadas do corpo da solicitação
+      const { title, description, price, typeName } = UpdateCoffeeSchema.parse(
+        req.body
+      );
+
+      // Verificar se o tipo de café está sendo atualizado
+      let typeId = existingCoffee.typeId;
+      if (typeName) {
+        const existingType = await prisma.type.findUnique({
+          where: {
+            title: typeName,
+          },
+        });
+
+        if (!existingType) {
+          return res
+            .status(400)
+            .send({ error: "The coffee type informed does not exist." });
+        }
+
+        typeId = existingType.id;
+      }
+
+      // Mesclar as propriedades atualizadas com as existentes
       const updatedCoffee = await prisma.coffee.update({
         where: {
           id,
         },
         data: {
-          title,
-          description,
-          price,
-          typeId: existingType.id,
+          title: title || existingCoffee.title,
+          description: description || existingCoffee.description,
+          price: price || existingCoffee.price,
+          typeId,
         },
       });
 
       return res.status(200).send(updatedCoffee);
     } catch (error) {
+      console.log(error);
       return res.status(500).send({ error: "Error updating coffee." });
     }
   });
--- server/src/schemas/coffee.ts
+++ server/src/schemas/coffee.ts
@@ -1,8 +1,15 @@
 import { number, object, string } from "zod";
 
-export const CoffeeSchema = object({
+export const CreateCoffeeSchema = object({
   title: string(),
   description: string(),
   price: number(),
   typeName: string(),
+});
+
+export const UpdateCoffeeSchema = object({
+  title: string().optional(),
+  description: string().optional(),
+  price: number().optional(),
+  typeName: string().optional(),
 });
