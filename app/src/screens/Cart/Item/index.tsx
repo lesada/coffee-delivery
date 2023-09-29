32e6d825458ada713734bf138ad4cfbe157956f4
Lauren Leslie Fereira <laurenlesliesilva@gmail.com>
2023-09-29 10:31:08 -0300

    feat: item layout

--- app/src/screens/Cart/Item/index.tsx
+++ app/src/screens/Cart/Item/index.tsx
@@ -1,12 +1,56 @@
-import Quantity from '@/components/Quantity';
+import { Feather } from '@expo/vector-icons';
+import { View } from 'react-native';
 
-import { Wrapper } from './styles';
+import { Images } from '@/assets';
+import Quantity from '@/components/Quantity';
+import Typography from '@/components/Typography';
+import theme from '@/styles/theme';
+
+import {
+  Action,
+  Container,
+  DeleteButton,
+  Photo,
+  Price,
+  Wrapper,
+} from './styles';
 
 function Item() {
   return (
-    <Wrapper>
-      <Quantity border />
-    </Wrapper>
+    <Container>
+      <Photo source={Images.AmericanoCoffee} />
+      <Wrapper>
+        <View>
+          <Typography size="large" type="text" color="neutral" variation={100}>
+            Irish Coffee
+          </Typography>
+          <Typography size="medium" type="text" color="neutral" variation={400}>
+            224ml
+          </Typography>
+        </View>
+        <Action>
+          <Quantity border />
+          <DeleteButton>
+            <Feather
+              name="trash-2"
+              size={20}
+              color={theme.colors.primary[200]}
+            />
+          </DeleteButton>
+        </Action>
+      </Wrapper>
+      <Price>
+        <Typography
+          size="medium"
+          type="title"
+          color="neutral"
+          variation={100}
+          bold
+        >
+          $ 3.50
+        </Typography>
+      </Price>
+    </Container>
   );
 }
 
--- app/src/screens/Cart/Item/styles.ts
+++ app/src/screens/Cart/Item/styles.ts
@@ -1,11 +1,44 @@
-import { View } from 'react-native';
+import { Image, TouchableOpacity, View } from 'react-native';
 import styled from 'styled-components';
 
-export const Wrapper = styled(View)`
+export const Container = styled(View)`
   flex-direction: row;
+  align-items: center;
   padding: 16px 32px;
   gap: 20px;
   flex-shrink: 0;
   border-bottom-width: 1px;
   border-bottom-color: ${({ theme }) => theme.colors.neutral[700]};
+`;
+
+export const Photo = styled(Image)`
+  width: 64px;
+  height: 64px;
+`;
+
+export const Wrapper = styled(View)`
+  gap: 8px;
+  flex: 1;
+`;
+
+export const Action = styled(View)`
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const DeleteButton = styled(TouchableOpacity)`
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.neutral[700]};
+  border-radius: 6px;
+  padding: 8px;
+  aspect-ratio: 1;
+`;
+
+export const Price = styled(View)`
+  height: 100%;
+
+  align-items: flex-start;
+  justify-content: flex-start;
 `;
